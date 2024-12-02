32dbd7986e6ade819963a35ed193fed298dd3fba
Lisa Chowdhury <lisafahmidatasnim@gmail.com>
2024-12-01 23:27:52 -0500

    start project

---
+++ breakfastMenu.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Breakfast Menu</title>
+    <link rel="stylesheet" href="css\style2.css">
+</head>
+<body>
+    <div class="container">
+        <div class="menu-item">
+            <div class="image-placeholder"></div>
+            <button>Breakfast Item 1</button>
+        </div>
+        <div class="menu-item">
+            <div class="image-placeholder"></div>
+            <button>Breakfast Item 2</button>
+        </div>
+        <div class="menu-item">
+            <div class="image-placeholder"></div>
+            <button>Breakfast Item 3</button>
+        </div>
+        <div class="menu-item">
+            <div class="image-placeholder"></div>
+            <button>Breakfast Item 4</button>
+        </div>
+        <div class="menu-item">
+            <div class="image-placeholder"></div>
+            <button>Under 5$</button>
+        </div>
+        <div class="menu-item">
+            <div class="image-placeholder"></div>
+            <button>Kids Breakfast Menu</button>
+        </div>
+        <div class="menu-item main-menu">
+            <form action="index.php" method="GET">
+                        <button type="submit">Back to Main Menu</but>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>6-12 Nutritional Calculator</title>
+    <link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left-section">
+            <h2>Select Category</h2>
+            <div class="category">
+                <div class="category-card">
+                    <div class="image-placeholder"></div>
+                    <form action="breakfastMenu.php" method="GET">
+                        <button type="submit">Breakfast</but>
+                    </form>
+                </div>
+                <div class="category-card">
+                    <div class="image-placeholder"></div>
+                    <button>Lunch</button>
+                </div>
+                <div class="category-card">
+                    <div class="image-placeholder"></div>
+                    <button>Dinner</button>
+                </div>
+            </div>
+        </div>
+        <div class="right-section">
+            <h2>6-12 Nutritional Calculator</h2>
+            <div class="large"></div>
+            <div class="text-scroll">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
+            </div>
+            <div class="buttons">
+                <button>About Us</button>
+                <button>Link to Website</button>
+                <button>Nutritional Information</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
