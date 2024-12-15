b1fc95e7748a70a44ebc2180c6076e1d9a34451d
Lisa Chowdhury <lisafahmidatasnim@gmail.com>
2024-12-15 17:06:13 -0500

    main menu page css

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>6-12 Nutritional Calculator</title>
+    <title>Nutritional Calculator</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style1.css">
 </head>
 <body>
@@ -12,28 +13,37 @@
             <h2>Select Category</h2>
             <div class="category">
                 <div class="category-card">
-                    <div class="image-placeholder"></div>
+                <div class="image-placeholder">
+                    <img src="images/hoagie2.jpg" alt="hoagie" class="image-placeholder">
+                </div>
+
                     <form action="breakfastMenu.php" method="GET">
-                        <button type="submit">Breakfast</but>
+                        <button type="submit">Hoagies</but>
                     </form>
                 </div>
                 <div class="category-card">
-                    <div class="image-placeholder"></div>
+                    <div class="image-placeholder">
+                        <img src="images/sides.jpg" alt="sides" class="image-placeholder"/>
+                    </div>
                     <form action="lunchMenu.php" method="GET">
-                    <button>Lunch</button>
+                    <button>Sides</button>
                     </form>
                 </div>
                 <div class="category-card">
-                    <div class="image-placeholder"></div>
+                    <div class="image-placeholder">
+                        <img src="images/drinks.jpg" alt="drinks" class="image-placeholder"/>
+                    </div>
                     <form action="dinnerMenu.php" method="GET">
-                    <button>Dinner</button>
+                    <button>Soft Drinks</button>
                     </form>
                 </div>
             </div>
         </div>
         <div class="right-section">
-            <h2>6-12 Nutritional Calculator</h2>
-            <div class="large"></div>
+            <h2>Nutritional Calculator</h2>
+            <div class="large">
+                <img src="images/hoagie3.jpg" class="large"/>
+            </div>
             <div class="text-scroll">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor.</p>
